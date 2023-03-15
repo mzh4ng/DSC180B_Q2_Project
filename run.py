aef24f8e410987219fec8307ead006b57e328d58
Benjamin Sacks <nadoben21@gmail.com>
2023-03-14 22:32:07 -0700

    Moved PCA to notebook

--- run.py
+++ run.py
@@ -10,58 +10,6 @@ from src.models import train_model
 from src.visualizations import visualize
 
 
-def pca(args):
-    if "test" in args:
-        metadata_filename = "data/test/test_metadata.tsv"
-        counts_filename = "data/test/test_fungi.tsv"
-        print("Using Test Data.")
-    else:
-        counts_filename = 'data/count_data_species_raw_WIS_overlapping_fungi_bacteria_12773samples.tsv'
-        metadata_filename = 'data/metadata_species_WIS_overlapping_fungi_bacteria_12773samples.tsv'
-
-    # load fungi counts and metadata into
-    counts = make_dataset.read_data_file(counts_filename)
-    raw_metadata = make_dataset.read_data_file(metadata_filename)
-
-    metadata = raw_metadata.replace('Not available', np.nan)
-    for col in ['pathologic_t_label', 'pathologic_n_label', 'pathologic_stage_label']:
-        metadata[col] = data_cleaning.reduce_stages(metadata[col])
-
-    target_column = sys.argv[2]
-
-    stages = ['Stage I', 'Stage II', 'Stage III', 'Stage IV']
-
-    # filter out missing stage data
-    metadata = metadata[metadata.pathologic_stage_label.isin(["Stage I", "Stage II", "Stage III", "Stage IV"])]
-
-    # filter to one experimental strategy
-    metadata = metadata[metadata.experimental_strategy == 'WGS']
-
-    # order by cancer stage
-    metadata['pathologic_stage_label'] = pd.Categorical(metadata['pathologic_stage_label'], categories=stages)
-    metadata = metadata.sort_values(by='pathologic_stage_label')
-
-    # remove cancer stage from pca (keep for coloring plot later)
-    targets = metadata[[target_column]]
-    metadata.drop(target_column, axis=1)
-
-    # preprocess metadata
-    metadata = build_features.preprocess_metadata(config, metadata)
-    metadata = metadata.iloc[:, :-7]
-
-    # merge counts data to metadata (drop any counts missing from index in metadata)
-    data = pd.merge(metadata, counts, on="sampleid", how="left")
-
-    pca = visualize.create_pca(metadata, targets, target_column)
-
-        # print()
-        # print("----  PC1 Loading Scores:  ----")
-        # print(pca[0][:5])
-        # print()
-        # print("----  PC2 Loading Scores:  ----")
-        # print(pca[0][:5])
-
-
 def main(config):
     """
     Main function to run the ML pipeline. Pipeline tuned based upon the inputted config file.
--- src/visualizations/visualize.py
+++ src/visualizations/visualize.py
@@ -3,9 +3,6 @@ import seaborn as sns
 import pandas as pd
 import os
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-
 # Set default font for graphs
 font = {'family': 'DejaVu Sans',
         'weight': 'bold',
@@ -78,64 +75,3 @@ def plot_boxplot(title, file_name, data_dict):
     plt.savefig(file_name)
     plt.close(fig)
     return plt
-
-
-def create_pca(data, targets, target_column):
-    """
-    """
-    # idx = np.random.permutation(data.index)
-    # data = data.reindex(idx)[:500]
-    # targets = targets.reindex(idx)[:500]
-    # # Run MDS
-    # ## 
-    # from sklearn.metrics import pairwise_distances
-    # from sklearn.manifold import MDS
-
-    # import warnings
-    # from sklearn.exceptions import DataConversionWarning
-    # warnings.filterwarnings(action='ignore', category=DataConversionWarning)
-
-    # distance_metric = 'jaccard'
-
-    # distances = pairwise_distances(data.to_numpy(), metric=distance_metric)
-    # pd.DataFrame(distances).to_csv('distances.csv')
-    # mds = MDS(dissimilarity='precomputed', random_state=0, normalized_stress=False)
-    # pca_data = mds.fit_transform(distances)
-
-    # Run PCA
-    ## Standardize data
-    scaler = StandardScaler()
-    scaled_data = scaler.fit_transform(data)
-    ## Run sklearn PCA on data
-    pca = PCA(n_components=2)
-    pca_data = pca.fit_transform(scaled_data)
-    # Convert ndarray to DataFrame, Add targets back in
-    pca_data = pd.DataFrame(pca_data, index=data.index)
-    pca_data = pd.merge(pca_data, targets, on="sampleid", how="inner")
-
-    # Plot PCA
-    ## Create scatterplot
-
-    sns.scatterplot(x=0, y=1, data=pca_data,
-                    hue=target_column,
-                    alpha=0.3)
-    plt.title("PCoA")
-    plt.legend(bbox_to_anchor=(1.01, 1),
-               loc=2,
-               borderaxespad=0.,
-               title=target_column)
-    ## Format PC labels to include explained variance
-    plt.xlabel(f'PC1 ({round(pca.explained_variance_[0], 2)}%)')
-    plt.ylabel(f'PC2 ({round(pca.explained_variance_[1], 2)}%)')
-    ## Save fig
-    plt.savefig("figures\pca\PCA_" + target_column + ".png", bbox_inches='tight')
-
-    # Return loading scores
-    ## Get loading scores from both PC's
-    pc1_loading_scores = pd.Series(pca.components_[0], index=data.columns)
-    pc2_loading_scores = pd.Series(pca.components_[1], index=data.columns)
-    ## Sort loading scores by magnitude
-    pc1_loading_scores = pc1_loading_scores.abs().sort_values(ascending=False)
-    pc2_loading_scores = pc2_loading_scores.abs().sort_values(ascending=False)
-    ## Return extracted series
-    return pc1_loading_scores, pc2_loading_scores
